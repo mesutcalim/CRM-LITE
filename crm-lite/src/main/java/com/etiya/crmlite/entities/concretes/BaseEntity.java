824095ec470ce4dda9d5e4e730a0a37f400ed51b
Veysel Kurnaz <veysel.kurnaz@etiya.com>
2022-12-19 18:29:13 +0300

    Added Party Class

---
+++ crm-lite/src/main/java/com/etiya/crmlite/entities/concretes/BaseEntity.java
@@ -0,0 +1,18 @@
+package com.etiya.crmlite.entities.concretes;
+
+import lombok.AllArgsConstructor;
+
+import lombok.NoArgsConstructor;
+
+import javax.persistence.MappedSuperclass;
+
+
+@AllArgsConstructor
+@NoArgsConstructor
+
+@MappedSuperclass
+public class BaseEntity {
+
+
+
+}
--- crm-lite/src/main/java/com/etiya/crmlite/entities/concretes/Party.java
+++ crm-lite/src/main/java/com/etiya/crmlite/entities/concretes/Party.java
@@ -1,4 +1,19 @@
 package com.etiya.crmlite.entities.concretes;
 
+
+import javax.persistence.*;
+
 public class Party {
+
+    @Id
+    @SequenceGenerator(name = "partyRoleSeq", sequenceName = "PARTY_ROLE_SEQ",allocationSize = 1)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE,generator = "partyRoleSeq")
+    @Column(name = "PARTY_ROLE_ID")
+    private Long partyRoleId;
+
+    @Column(name="PARTY_TP_ID")
+    private Long partyTpId;
+
+    @Column(name="ST_ID")
+    private int stId;
 }
