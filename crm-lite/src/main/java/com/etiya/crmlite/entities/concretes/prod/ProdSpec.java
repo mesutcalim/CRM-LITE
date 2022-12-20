5b866b145356724a2632e971d93863c0750065d6
AbdulkadirDemirel <abdulkadir.demirel@etiya.com>
2022-12-21 01:13:54 +0300

    Changes by sametcan.sertman

---
+++ crm-lite/src/main/java/com/etiya/crmlite/entities/concretes/prod/ProdSpec.java
@@ -0,0 +1,47 @@
+package com.etiya.crmlite.entities.concretes.prod;
+
+import com.etiya.crmlite.entities.abstracts.BaseEntity;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+
+import javax.persistence.*;
+import java.util.List;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Entity
+@Table(name="PROD_SPEC")
+public class ProdSpec extends BaseEntity {
+    @Id
+    @SequenceGenerator(name = "prodSpecSeq", sequenceName = "PROD_SPEC_SEQ", allocationSize = 1)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "prodSpecSeq")
+    @Column (name="PROD_SPEC_ID")
+    private Long prodSpecId;
+    
+    @Column (name="NAME")
+    private String name;
+    
+    @Column (name="DESCR")
+    private String descr;
+    
+    @OneToMany (mappedBy = "prodSpec")
+    private List<ProdOfr> prodOfrs;
+    
+    @Column (name="IS_DEV")
+    private int isDev;
+
+    @OneToMany (mappedBy = "prodSpec")
+    private List<Prod> prods;
+
+    @OneToMany(mappedBy = "prodSpec")
+    private List<ProdSpecRsrcSpec> prodSpecRsrcSpecs;
+
+    @OneToMany(mappedBy = "prodSpec")
+    private List<ProdSpecSrvcSpec> prodSpecSrvcSpecs;
+
+    @OneToMany(mappedBy = "prodSpec")
+    private List<ProdSpecCharUse> prodSpecCharUses;
+}
