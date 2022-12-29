77894824df791111bd5e05e9f4f10d172c8298a9
Mesut Çalım <mesut.calim@etiya.com>
2022-12-29 20:11:05 +0300

    edited(customerRepository,individualRepository)

--- crm-lite/src/main/java/com/etiya/crmlite/repositories/cam/ICustomerRepository.java
+++ crm-lite/src/main/java/com/etiya/crmlite/repositories/cam/ICustomerRepository.java
@@ -2,30 +2,35 @@ package com.etiya.crmlite.repositories.cam;
 
 
 import com.etiya.crmlite.business.dtos.responses.cam.customers.FindCustomerResponse;
+import com.etiya.crmlite.business.dtos.responses.product.products.GetAllProductResponse;
 import com.etiya.crmlite.entities.concretes.cam.Cust;
 import com.etiya.crmlite.entities.concretes.prod.Prod;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+
 
 import java.util.List;
 
 public interface ICustomerRepository extends JpaRepository<Cust, Long> {
 
     @Query("SELECT DISTINCT NEW com.etiya.crmlite.business.dtos.responses.cam.customers.FindCustomerResponse " +
-            "(c.custId,i.frstName,i.mName,i.lstName,c.partyRole,i.natId) " +
+            "(c.custId,i.frstName,i.mName,i.lstName,prt.name,i.natId) " +
             "FROM Party p JOIN p.ind i " +
             "join p.partyRoles pr " +
             "join pr.cust c " +
             "join pr.partyRoleTp prt " +
             "join c.custAccts ca " +
             "join CustOrd co on co.custId = c.custId " +
-            "join CntcMedium cm on (cm.rowId = p.partyId and cm.dataTpId = 9 and cm.cntcMediumTpId = 172) " +
-            "where(i.natId = :natId or :natId is null) " +
+            "join CntcMedium cm on cm.rowId = p.partyId " +
+            "where" +
+            "(cm.dataTpId is null or cm.dataTpId = 9) " +
+            "and (cm.cntcMediumTpId is null or cm.cntcMediumTpId = 172) " +
+            "and (i.natId = :natId or :natId is null) " +
             "and (c.custId = :customerId or :customerId is null) " +
             "and (ca.acctNo = :accountNumber or :accountNumber is null) " +
             "and (cm.cntcData = :gsmNumber or :gsmNumber is null) " +
-            "and (:firstName is null or lower(i.frstName) LIKE CONCAT(lower(:firstName),'%' )) " +
-            "and (:lastName is null or lower(i.lstName) LIKE CONCAT(lower(:lastName),'%')) " +
+            "and (:firstName is null or lower(i.frstName) = lower(:firstName) ) " +
+            "and (:lastName is null or lower(i.lstName) = lower(:lastName)) " +
             "and (co.custOrdId = :customerOrderId or :customerOrderId is null) ")
     List<FindCustomerResponse> getCustomersByFilter(Long customerId,
                                                     String firstName,
@@ -37,9 +42,10 @@ public interface ICustomerRepository extends JpaRepository<Cust, Long> {
     );
 
 
-    @Query(value = "SELECT * FROM CUST_ACCT ca \n" +
-            "LEFT JOIN CUST c  ON ca.CUST_ID = c.CUST_ID  \n" +
-            "LEFT JOIN CUST_ACCT_PROD_INVL capi ON capi.CUST_ACCT_ID = ca.CUST_ACCT_ID \n" +
-            "LEFT JOIN PROD p ON capi.PROD_ID = p.PROD_ID WHERE p.ST_ID IN (1500,116,10600) AND c.CUST_ID = :customerId", nativeQuery = true)
-    List<Prod> checkActiveProduct(Long customerId);
+    @Query(value = "SELECT * FROM "+
+            " FROM CUST_ACCT ca " +
+            "JOIN CUST  c ON ca.CUST_ID = c.CUST_ID " +
+            "JOIN CUST_ACCT_PROD_INVL capi ON capi.CUST_ACCT_ID = ca.CUST_ACCT_ID " +
+            "JOIN PROD p ON capi.PROD_ID = p.PROD_ID WHERE p.ST_ID IN (1500,116,10600) AND c.CUST_ID = 615 ", nativeQuery = true)
+    List<GetAllProductResponse> checkActiveProduct(Long customerId);
 }
--- crm-lite/src/main/java/com/etiya/crmlite/repositories/cam/IIndividualRepository.java
+++ crm-lite/src/main/java/com/etiya/crmlite/repositories/cam/IIndividualRepository.java
@@ -4,4 +4,5 @@ import com.etiya.crmlite.entities.concretes.cam.Ind;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface IIndividualRepository extends JpaRepository<Ind,Long> {
+    Ind findByNatId(Long nationalityId);
 }
